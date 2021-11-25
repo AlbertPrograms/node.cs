7ec4298042dbb8c20c53cf3e4923e37afdaabba1
Albert <midnyte25@gmail.com>
2021-11-25 15:13:23 +0100

    Backend working with node.cs_server compiler and runner service

--- client/src/components/Editor.tsx
+++ client/src/components/Editor.tsx
@@ -1,26 +1,73 @@
+import { ChangeEventHandler, FormEventHandler, useState } from 'react';
 import './Editor.css';
 
 interface EditorProps {
   height: number;
 }
 
+interface TaskResponse {
+  task: string;
+  token: string;
+}
+
 const getRowsByHeight = (height: number) => {
   return Math.floor(height / 25) - 8;
 };
 
-const Editor: React.FC<EditorProps> = ({ height }) => (
-  <div className="container d-flex vh-100">
-    <div className="row w-100 justify-content-center align-self-center">
-      <form method="post" action="/compile">
-        <textarea rows={getRowsByHeight(height)} name="code" className="form-control" />
-        <div className="row justify-content-center">
-          <div className="col col-4 col-md-2">
-            <button className="form-control btn">Beküldés</button>
+const Editor: React.FC<EditorProps> = ({ height }) => {
+  const [task, setTask] = useState<string>();
+  const [code, setCode] = useState<string>();
+  let token: string;
+
+  fetch('/get-task')
+    .then((res) => res.json())
+    .then((r: TaskResponse) => {
+      setTask(r.task);
+      token = r.token;
+    });
+
+  const handleTextareaChange: ChangeEventHandler = (event) => {
+    const textarea: HTMLTextAreaElement = event.target as HTMLTextAreaElement;
+    setCode(textarea.value);
+  };
+
+  const handleSubmit: FormEventHandler = (event) => {
+    event.preventDefault();
+
+    fetch('/submit-task', {
+      method: 'POST',
+      body: JSON.stringify({
+        token,
+        code,
+      }),
+    })
+      .then((res) => res.json())
+      .then((r: TaskResponse) => {
+        setTask(r.task);
+        token = r.token;
+      });
+  };
+
+  return (
+    <div className="container d-flex vh-100">
+      <div className="row w-100 justify-content-center align-self-center">
+        {task ? task : ''}
+        <form method="post" onSubmit={handleSubmit}>
+          <textarea
+            rows={getRowsByHeight(height)}
+            name="code"
+            className="form-control"
+            onChange={handleTextareaChange}
+          />
+          <div className="row justify-content-center">
+            <div className="col col-4 col-md-2">
+              <button className="form-control btn">Beküldés</button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Editor;
--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,17 @@
 import express from 'express';
-import { c } from 'compile-run';
+import bcrypt from 'bcrypt';
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const compilerAddress = process.env.compilerAddress;
+
+if (!compilerAddress) {
+  throw new Error('Compiler address must be set in ".env"!');
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,7 +24,7 @@ const init = async (): Promise<void> => {
   }
 
   let resolve: () => void;
-  initPromise = new Promise(r => (resolve = r));
+  initPromise = new Promise((r) => (resolve = r));
 
   // TODO
 
@@ -28,13 +37,68 @@ let initialized = false;
 
 init();
 
+type TaskToken = {
+  id: Task['id'];
+  token: string;
+};
+
+type TaskCategory = '' | '';
+
+type Task = {
+  id: number;
+  description: string;
+  testData?: [string | number];
+  expectedResult: string | string[];
+  pointValue: number;
+};
+
+const taskTokens: TaskToken[] = [];
+
+const getTaskToken = () => bcrypt.hashSync(new Date().getTime().toString(), 5);
+
+// TODO type, separate file (maybe from input json?)
+const tasks: Task[] = [
+  {
+    id: 1,
+    description: 'Say "Hello world!"',
+    expectedResult: 'Hello world',
+    pointValue: 1,
+  },
+];
+
 app.use(async (_, __, next) => {
   await initPromise;
   next();
-})
+});
+
+app.get('/get-task', (_, res) => {
+  const token = getTaskToken();
+  taskTokens.push({
+    id: tasks[0].id,
+    token,
+  });
+
+  res.send({
+    task: tasks[0].description,
+    token,
+  });
+});
+
+app.post('/submit-task');
 
 app.post('/compile', async (req, res) => {
   const code = req.body.code;
-  c.runSource(code);
-  res.status(200);
+  const response = await fetch(`http://${compilerAddress}/compile-and-run`, {
+    method: 'POST',
+    body: JSON.stringify({
+      code,
+    }),
+  });
+  console.log(response);
+
+  if (!response.status.toString().startsWith('2')) {
+    res.status(response.status).send(response);
+  } else {
+    res.status(200);
+  }
 });
