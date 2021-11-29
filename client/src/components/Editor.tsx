c8764ea27538e1d5e8322f33f5c19bd5d2fb26cc
Albert <midnyte25@gmail.com>
2021-11-29 22:22:06 +0100

    Test data capability added

--- client/src/components/Editor.tsx
+++ client/src/components/Editor.tsx
@@ -1,4 +1,9 @@
-import { ChangeEventHandler, FormEventHandler, useState } from 'react';
+import {
+  ChangeEventHandler,
+  MouseEventHandler,
+  FormEventHandler,
+  useState,
+} from 'react';
 import './Editor.css';
 
 interface EditorProps {
@@ -10,6 +15,18 @@ interface TaskResponse {
   token: string;
 }
 
+interface ExecutionResult {
+  code: number;
+  args?: string;
+  stdout: string;
+  stderr: string;
+}
+
+interface SubmitResponse {
+  results: ExecutionResult[];
+  success: boolean;
+}
+
 const getRowsByHeight = (height: number) => {
   return Math.floor(height / 25) - 8;
 };
@@ -17,6 +34,7 @@ const getRowsByHeight = (height: number) => {
 const Editor: React.FC<EditorProps> = ({ height }) => {
   const [task, setTask] = useState<string>();
   const [code, setCode] = useState<string>();
+  const [submitResponse, setSubmitResponse] = useState<SubmitResponse>(); // TODO
   let token: string;
 
   fetch('/get-task')
@@ -31,8 +49,21 @@ const Editor: React.FC<EditorProps> = ({ height }) => {
     setCode(textarea.value);
   };
 
+  const handleSubmitError = (res: SubmitResponse) => {
+    console.log(res);
+    setSubmitResponse(res);
+  };
+
+  const handleSubmitSuccess = (res: SubmitResponse) => {
+    console.log(res);
+    setSubmitResponse(res);
+  };
+
+  // TODO cookie token as session
+  // TODO store progress on BE every couple of seconds with useEffect
   const handleSubmit: FormEventHandler = (event) => {
     event.preventDefault();
+    console.log(token);
 
     fetch('/submit-task', {
       method: 'POST',
@@ -40,29 +71,89 @@ const Editor: React.FC<EditorProps> = ({ height }) => {
         token,
         code,
       }),
-    })
-      .then((res) => res.json())
-      .then((r: TaskResponse) => {
-        setTask(r.task);
-        token = r.token;
-      });
+      headers: { 'Content-Type': 'application/json' },
+    }).then(async (res) => {
+      const response = (await res.json()) as SubmitResponse;
+      if (!res.status.toString().startsWith('2') || !response.success) {
+        handleSubmitError(response);
+      } else {
+        handleSubmitSuccess(response);
+      }
+    });
   };
 
+  const handleNext: MouseEventHandler = (event) => {
+    event.preventDefault();
+  };
+
+  // TODO monospace ``
   return (
     <div className="container d-flex vh-100">
       <div className="row w-100 justify-content-center align-self-center">
-        {task ? task : ''}
+        <h4>{task ? task : ''}</h4>
         <form method="post" onSubmit={handleSubmit}>
-          <textarea
-            rows={getRowsByHeight(height)}
-            name="code"
-            className="form-control"
-            onChange={handleTextareaChange}
-          />
+          <div className="row">
+            <div className={`col ${submitResponse ? 'col-6' : 'col-12'}`}>
+              <textarea
+                rows={getRowsByHeight(height)}
+                name="code"
+                className="form-control h-100 bg-dark text-light font-monospace"
+                onChange={handleTextareaChange}
+                spellCheck={false}
+              />
+            </div>
+            {submitResponse && (
+              <div className="col col-6">
+                <div className="card h-100 bg-dark border-light px-3 py-1 font-monospace">
+                  {submitResponse.results.map((result) => (
+                    <div>
+                      {result.args && <div className="row">
+                        <div className="lead mb-2">Futtatási argumentumok</div>
+                        <div>{result.args}</div>
+                      </div>}
+                      <div className="row">
+                        <div className="lead mb-2">Kimenet</div>
+                        <div>{result.stdout}</div>
+                      </div>
+                      {result.stderr && (
+                        <div className="row">
+                          <hr />
+                          <div className="text-danger lead my-2">
+                            Hibakimenet
+                          </div>
+                          <div>{result.stderr}</div>
+                        </div>
+                      )}
+                      <div
+                        className={`row${
+                          result.code !== 0 ? ' text-danger' : ''
+                        }`}
+                      >
+                        <hr />
+                        <div className="mt-2">Kilépőkód: {result.code}</div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
           <div className="row justify-content-center">
             <div className="col col-4 col-md-2">
-              <button className="form-control btn">Beküldés</button>
+              <button className="form-control btn btn-dark mt-2">
+                Beküldés
+              </button>
             </div>
+            {submitResponse?.success && (
+              <div className="col col-4 col-md-2">
+                <button
+                  onClick={handleNext}
+                  className="form-control btn btn-dark mt-2"
+                >
+                  Továbblépés
+                </button>
+              </div>
+            )}
           </div>
         </form>
       </div>
--- server/index.ts
+++ server/index.ts
@@ -37,20 +37,37 @@ let initialized = false;
 
 init();
 
-type TaskToken = {
+interface TaskToken {
   id: Task['id'];
   token: string;
-};
+}
 
 type TaskCategory = '' | '';
 
-type Task = {
+interface Task {
   id: number;
   description: string;
-  testData?: [string | number];
-  expectedResult: string | string[];
+  testData?: string[];
+  expectedResults: string[];
   pointValue: number;
-};
+}
+
+interface ExecutionResult {
+  code: number;
+  args?: string;
+  stdout: string;
+  stderr: string;
+}
+
+interface SubmitTaskRequest {
+  code: string;
+  token: string;
+}
+
+interface SubmitTaskResponse {
+  results: ExecutionResult[];
+  success: boolean;
+}
 
 const taskTokens: TaskToken[] = [];
 
@@ -59,46 +76,85 @@ const getTaskToken = () => bcrypt.hashSync(new Date().getTime().toString(), 5);
 // TODO type, separate file (maybe from input json?)
 const tasks: Task[] = [
   {
-    id: 1,
-    description: 'Say "Hello world!"',
-    expectedResult: 'Hello world',
+    description: 'Írassa ki a standard kimenetre, hogy `Hello world!`',
+    expectedResults: ['Hello world!'],
     pointValue: 1,
   },
-];
+  {
+    description:
+      'Írjon programot, mely az argumentumban megadott sorszámú fibonacci sorozatot írja ki vesszővel és szóközzel elválasztva! Pl. bemenet: 7, kimenet: `1, 1, 2, 3, 5, 8, 13`',
+    testData: ['1', '10', '30'],
+    expectedResults: [
+      '1',
+      '1, 1, 2, 3, 5, 8, 13, 21, 34, 55',
+      '1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040',
+    ],
+    pointValue: 3,
+  },
+].map((task, index) => ({ ...task, id: index }));
 
 app.use(async (_, __, next) => {
   await initPromise;
   next();
 });
 
+// TODO random task assignment based on category and practice mode
 app.get('/get-task', (_, res) => {
+  // TEMP
+  const taskId = 1;
+
   const token = getTaskToken();
   taskTokens.push({
-    id: tasks[0].id,
+    id: tasks[taskId].id,
     token,
   });
 
   res.send({
-    task: tasks[0].description,
+    task: tasks[taskId].description,
     token,
   });
 });
 
-app.post('/submit-task');
+const handleResults = (
+  results: ExecutionResult[],
+  task: Task
+): SubmitTaskResponse => {
+  const success = results.every(
+    (result, index) => result.stdout === task.expectedResults[index]
+  );
+  // TODO handle task completion to user
+  return { results, success };
+};
 
-app.post('/compile', async (req, res) => {
-  const code = req.body.code;
+app.post('/submit-task', async (req, res) => {
+  const { code, token }: SubmitTaskRequest = req.body;
+  console.log(code);
+  const taskToken = taskTokens.find((tt) => tt.token === token);
+
+  if (!taskToken) {
+    res.status(400).send('Invalid token');
+    return;
+  }
+
+  const task = tasks[taskToken.id];
   const response = await fetch(`http://${compilerAddress}/compile-and-run`, {
     method: 'POST',
     body: JSON.stringify({
       code,
     }),
+    headers: { 'Content-Type': 'application/json' },
   });
-  console.log(response);
 
-  if (!response.status.toString().startsWith('2')) {
-    res.status(response.status).send(response);
+  const results = (await response.json()) as ExecutionResult[];
+
+  console.log(task);
+  const submitResponse: SubmitTaskResponse = handleResults(results, task);
+  res.send(submitResponse);
+
+  /* if (!response.status.toString().startsWith('2')) {
+    // Provide next task
+    res.status(response.status).send(results);
   } else {
-    res.status(200);
-  }
+    res.status(200).send(results);
+  } */
 });
