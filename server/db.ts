20275ece91a1ed8601484236fa21ee9b876d0bd7
Albert <midnyte25@gmail.com>
2021-12-13 21:41:53 +0100

    Add PostgreSQL support

--- server/db.ts
+++ server/db.ts
@@ -1,34 +1,52 @@
 import path from 'path';
 import fs from 'fs';
-import knex from 'knex';
+import knex, { Knex } from 'knex';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const dbArraySeparatorString = '__,__';
 
-const dbFolder = path.resolve(__dirname, 'db');
-const dbPath = path.resolve(__dirname, 'db/database.sqlite');
+let db: Knex<any, unknown>;
 
-// Create DB folder if it doesn't already exist
-if (!fs.existsSync(dbFolder)) {
-  fs.mkdirSync(dbFolder);
-}
-
-// Create DB file if it doesn't already exist
-fs.open(dbPath, 'r', err => {
-  if (!err) return;
-
-  fs.writeFile(dbPath, '', err => {
-    console.log(dbPath);
-    console.error(err);
+if (process.env.DATABASE_URL) {
+  // Postgres
+  db = knex({
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
   });
-});
+} else if (process.env.db === 'sqlite3') {
+  // SQLite
+  const dbFolder = path.resolve(__dirname, 'db');
+  const dbPath = path.resolve(__dirname, 'db/database.sqlite');
 
-// Create connection to SQLite database
-const db = knex({
-  client: 'sqlite3',
-  connection: {
-    filename: dbPath,
-  },
-  useNullAsDefault: true,
-});
+  // Create DB folder if it doesn't already exist
+  if (!fs.existsSync(dbFolder)) {
+    fs.mkdirSync(dbFolder);
+  }
+
+  // Create DB file if it doesn't already exist
+  fs.open(dbPath, 'r', err => {
+    if (!err) return;
+
+    fs.writeFile(dbPath, '', err => {
+      console.error(err);
+    });
+  });
+  
+  // Create connection to SQLite database
+  db = knex({
+    client: 'sqlite3',
+    connection: {
+      filename: dbPath,
+    },
+    useNullAsDefault: true,
+  });
+} else {
+  throw new Error('no valid database config found');
+}
 
 export { db, dbArraySeparatorString };
