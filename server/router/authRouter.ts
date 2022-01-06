995bd32fb6b6ec8211e397e288b8ef6bfc021efe
Albert <midnyte25@gmail.com>
2022-01-06 21:00:27 +0100

    Routing reorganization

--- server/router/authRouter.ts
+++ server/router/authRouter.ts
@@ -26,6 +26,7 @@ const init = async () => {
   initialized = true;
   resolve();
 };
+init();
 
 /* --== Interfaces, methods and variables ==-- */
 
@@ -72,78 +73,17 @@ const removeSession = (tokenString: string): void => {
   sessions.splice(sessionIndex, 1);
 };
 
-/* --== Routes ==-- */
+/* --== Routing middleware ==-- */
 
-// User init awaiting,
-router.use(async (req, res, next) => {
-  await init();
-  const { sessionTokenString } = req.body;
-
-  if (sessionTokenString) {
-    // Check validity of token if provided, then store user and session in res.locals if user found
-    const user = getUserFromSessionTokenString(sessionTokenString);
-    if (user) {
-      res.locals.user = user;
-      res.locals.session = sessionTokenString;
-    }
-  }
-
-  next();
-});
-
-router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-  const user = userTable.find({ username });
-
-  if (!user) {
-    res.status(401).send();
-    return;
-  }
-
-  const authSuccess = user.authenticate(password);
-
-  if (!authSuccess) {
-    // Delay to protect from brute force
-    setTimeout(() => res.status(401).send(), 1500);
-    return;
-  }
-
-  const sessionTokenString = assignSession(user);
-  res.send({ sessionTokenString });
-});
-
-router.post('/validate-token', (_, res) => {
-  const valid = !!res.locals.user;
-  res.status(valid ? 200 : 401).send();
-});
-
-router.post('/logout', (req, res) => {
-  const { sessionTokenString } = req.body;
-
-  const valid = !!res.locals.user;
-
-  if (valid) {
-    removeSession(sessionTokenString);
-  }
-
-  res.status(valid ? 200 : 401).send();
-});
-
-router.post('/get-user-data', (req, res) => {
+const needsUser = (_: any, res: express.Response, next: express.NextFunction) => {
   const user = res.locals.user as User;
   if (!user) {
     res.status(401).send();
     return;
   }
 
-  res.send({
-    name: user.Name,
-    isAdmin: user.isAdmin(),
-    isTeacher: user.isTeacher(),
-  });
-});
-
-/* --== Routing middleware ==-- */
+  next();
+};
 
 const needsAdmin = (_: any, res: express.Response, next: express.NextFunction) => {
   const user = res.locals.user as User;
@@ -176,6 +116,77 @@ const needsTeacherOrAdmin = (_: any, res: express.Response, next: express.NextFu
   next();
 };
 
+/* --== Routes ==-- */
+
+// UserTable init await, user and session saving to res.locals
+router.use(async (req, res, next) => {
+  await initPromise;
+  const { sessionTokenString } = req.body;
+
+  if (sessionTokenString) {
+    // Check validity of token if provided, then store user and session in res.locals if user found
+    const user = getUserFromSessionTokenString(sessionTokenString);
+    if (user) {
+      res.locals.user = user;
+      res.locals.session = sessionTokenString;
+    }
+  }
+
+  next();
+});
+
+// Login
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = userTable.find({ username });
+
+  if (!user) {
+    res.status(401).send();
+    return;
+  }
+
+  const authSuccess = user.authenticate(password);
+
+  if (!authSuccess) {
+    // Delay to protect from brute force
+    setTimeout(() => res.status(401).send(), 1500);
+    return;
+  }
+
+  const sessionTokenString = assignSession(user);
+  res.send({ sessionTokenString });
+});
+
+// Token validation
+router.post('/validate-token', (_, res) => {
+  const valid = !!res.locals.user;
+  res.status(valid ? 200 : 401).send();
+});
+
+// Logout
+router.post('/logout', (req, res) => {
+  const { sessionTokenString } = req.body;
+
+  const valid = !!res.locals.user;
+
+  if (valid) {
+    removeSession(sessionTokenString);
+  }
+
+  res.status(valid ? 200 : 401).send();
+});
+
+// User data retrieval
+router.post('/get-user-data', needsUser, (_, res) => {
+  const user = res.locals.user as User;
+
+  res.send({
+    name: user.Name,
+    isAdmin: user.isAdmin(),
+    isTeacher: user.isTeacher(),
+  });
+});
+
 /* --== Intervals ==-- */
 
 // Check for expired session tokens every minute and delete them if they exist
@@ -189,5 +200,7 @@ setInterval(() => {
   }
 }, 60000);
 
+/* --== Exports ==-- */
+
 export default router;
-export { needsAdmin, needsTeacher, needsTeacherOrAdmin };
+export { needsUser, needsAdmin, needsTeacher, needsTeacherOrAdmin };
